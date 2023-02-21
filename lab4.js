737becd01148030d526861581a3b050fad526198
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-02-21 15:43:14 -0600

    svs

--- lab4.js
+++ lab4.js
@@ -1,1 +1,59 @@
-let 
+let answer1 ="";
+let answer2 = "";
+let answer3 = "";
+let answer4 = "";
+let answer5 = "";
+
+function test() {
+    answer1 = prompt("my name rena");
+    answer2 = prompt(" I play sims");
+    answer3 = prompt("I watch live act");
+    answer4 = prompt("I have a fatbike");
+    answer5 = prompt("I want to make a cake");
+    if(answer1 !== "y"){
+        alert("nope");
+       
+    }
+    else{
+        alert("good");
+        
+    }
+
+    if(answer2 !== "n"){
+        alert("nope");
+       
+    }
+    else{
+        alert("good");
+        
+    }
+
+    if(answer3 !== "n "){
+        alert("nope");
+       
+    }
+    else{
+        alert("good");
+        
+    }
+    
+    if(answer4 !== "y"){
+        alert("nope");
+       
+    }
+    else{
+        alert("good");
+        
+    }
+
+    if(answer5 !== "n"){
+        alert("nope");
+       
+    }
+    else{
+        alert("good");
+        
+    }
+    
+}
+
