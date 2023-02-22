82cfd5eb879caec817724ad1bf24b99584e46b7f
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-02-22 14:57:34 -0600

    Update lab4.js

--- lab4.js
+++ lab4.js
@@ -3,14 +3,21 @@ let answer2 = "";
 let answer3 = "";
 let answer4 = "";
 let answer5 = "";
+let trueAW = [ "y","n","n","y","n",4,"a"];
+let userAW = [];
 
 
 function test() {
     answer1 = prompt("my name rena   (y/n)");
+    userAW.push(answer1);
     answer2 = prompt(" I play sims   (y/n)");
+    userAW.push(answer2);
     answer3 = prompt("I watch live act   (y/n)");
+    userAW.push(answer3);
     answer4 = prompt("I have a fatbike   (y/n)");
+    userAW.push(answer4);
     answer5 = prompt("I want to make a cake   (y/n)");
+    userAW.push(answer5);
     
     if(answer1 !== "y"){
         alert("nope Im Rena");
@@ -50,14 +57,17 @@ function qust6(){
     
 
     answer6 = prompt("Pick a number between 1 and 10");
+    userAW.push(answer6);
 
     while(answer6 != 4){
         if(answer6 < 4){
             alert("too low try again");
             answer6 = prompt("number between 1 and 10");
+            userAW.push(answer6);
         }else if(answer6 > 4){
             alert("too high try again");
             answer6 = prompt("number between 1 and 10");
+            userAW.push(answer1);
         }
     }
     alert(" (/*￣︶￣)/ you did it");
@@ -71,6 +81,7 @@ function qust7(){
 
     while(tries > 0){
         const Uanswer7 = prompt("answer:")
+        userAW.push(Uanswer7);
         if(Uanswer7 == null){
             break;
         }else if(Uanswer7 == answer7){
@@ -83,6 +94,10 @@ function qust7(){
     if(!answerRight){
         alert('You out of tries!');
     }
+}
+
+function ranking(){
+
 }
 
 let username = "";
