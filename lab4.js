7d72847d7515789739e210ff96f2ffaafef57a44
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-02-22 11:47:40 -0600

    update

--- lab4.js
+++ lab4.js
@@ -4,39 +4,64 @@ let answer3 = "";
 let answer4 = "";
 let answer5 = "";
 
+
 function test() {
-    answer1.toLocaleLowerCase = prompt("my name rena (y\n)");
-    answer2.toLocaleLowerCase = prompt(" I play sims (y\n)");
-    answer3.toLocaleLowerCase = prompt("I watch live act (y\n)");
-    answer4.toLocaleLowerCase = prompt("I have a fatbike (y\n)");
-    answer5.toLocaleLowerCase = prompt("I want to make a cake (y\n)");
+    answer1.toLocaleLowerCase = prompt("my name rena   (y/n)");
+    answer2.toLocaleLowerCase = prompt(" I play sims   (y/n)");
+    answer3.toLocaleLowerCase = prompt("I watch live act   (y/n)");
+    answer4.toLocaleLowerCase = prompt("I have a fatbike   (y/n)");
+    answer5.toLocaleLowerCase = prompt("I want to make a cake   (y/n)");
+    
     if(answer1 !== "y"){
         alert("nope Im Rena");
     }else{
         alert("Ya Im rena");
     }
+
     if(answer2 !== "n"){
         alert("I don't play sims");
     }else{
         alert("It show time!");
     }
+
     if(answer3 !== "n "){
         alert("Im not big on live action");
     }else{
         alert("Animations are cooler");
     }
+
     if(answer4 !== "y"){
         alert("nope i have a fatbike");
     }else{
         alert("I love my ebike");
     }
+
     if(answer5 !== "n"){
         alert("nope im a lazy cook");
     }else{
         alert("Its my dream job");
+
     }
     
+    qust6();
 }
+
+function qust6(){
+    answer6 = prompt("Pick a number between 1 and 10");
+
+    while(answer6 != 4){
+        if(answer6 < 4){
+            alert("too low try again");
+            answer6 = prompt("number between 1 and 10");
+        }else if(answer6 > 4){
+            alert("too high try again");
+            answer6 = prompt("number between 1 and 10");
+        }
+    }
+    alert(" (/*￣︶￣)/ you did it \(￣︶￣*\) ");
+}
+
+
 
 let username = "";
 
@@ -44,7 +69,7 @@ function signin() {
     username = prompt("Username?");
     if(username != ''){
         alert("Hello, " + username);
-        document.getElementById("user").innerHTML = " (￣︶￣) " + username;
+        document.getElementById("user").innerHTML = " (￣︶￣) WELCOME " + username;
     } 
     else {
         alert('Iniviled User');
